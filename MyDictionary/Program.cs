5479d6efee10a5cb925e112e44c920d43dc885e6
Samet Kayahan <skayahan1@outlook.com>
2021-01-23 20:26:53 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> icecekler = new MyDictionary<string>();
+            icecekler.Add("Kola");
+            icecekler.Add("Ayran");
+            icecekler.Add("Fanta");
+        }
+    }
+}
